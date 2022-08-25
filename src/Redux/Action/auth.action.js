ff57b10035109832b1d46c5ce9e5f071239b9cd8
Rutvi_Kapuriya® <106071932+rutvikapuriya9161@users.noreply.github.com>
2022-08-25 13:27:14 +0530

    Update auth.action.js

--- src/Redux/Action/auth.action.js
+++ src/Redux/Action/auth.action.js
@@ -3,3 +3,7 @@ import * as ActionTypes from '../ActionTypes';
 export const signUpAction = (data) => (dispatch) => {
     dispatch({type : ActionTypes.SIGN_UP , payload : data});
 }   
+
+export const signInAction = (data) => (dispatch) => {
+    dispatch({type : ActionTypes.SIGN_IN , payload : data})
+}
